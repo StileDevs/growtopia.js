9059491207a9a9acaa0471e63dffcc010c183395
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-07-06 19:15:31 +0700

    fix: nonstop server listening when testing done

--- test/basic-example/index.js
+++ test/basic-example/index.js
@@ -15,6 +15,8 @@ client.on("ready", () => {
   console.log(
     `ENet server: port ${client.config.enet.port} on ${client.config.enet.ip}\nHttps server: port ${client.config.https.httpsPort} on ${client.config.https.ip}`
   );
+  // Success
+  process.exit(1);
 });
 
 client.on("error", (err) => {
