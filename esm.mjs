7686b824b442be06bd3ee6cf6a3d91af2566973a
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2025-02-16 23:53:15 +0700

    2.0.14

--- esm.mjs
+++ esm.mjs
@@ -1,9 +1,9 @@
-const Client = await import("./dist/structures/Client.js");
-const Peer = await import("./dist/structures/Peer.js");
-const TankPacket = await import("./dist/packets/TankPacket.js");
-const TextPacket = await import("./dist/packets/TextPacket.js");
-const Variant = await import("./dist/packets/Variant.js");
-const ItemsDat = await import("./dist/utils/ItemsDat.js");
+const Client = (await import("./dist/structures/Client.js")).Client;
+const Peer = (await import("./dist/structures/Peer.js")).Peer;
+const TankPacket = (await import("./dist/packets/TankPacket.js")).TankPacket;
+const TextPacket = (await import("./dist/packets/TextPacket.js")).TextPacket;
+const Variant = (await import("./dist/packets/Variant.js")).Variant;
+const ItemsDat = (await import("./dist/utils/ItemsDat.js")).ItemsDat;
 const Constants = await import("./dist/Constants.js");
 
 export * from "./native.js";
